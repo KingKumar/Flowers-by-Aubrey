fdc174959fd8672de444a8257c2b48f0890fc4e6
KingKumar <kumi12345@gmail.com>
2026-05-03 17:20:04 -0700

    newest bouquets display first

--- components/flowerOfferings.ts
+++ components/flowerOfferings.ts
@@ -9,99 +9,91 @@ export type FlowerOffering = {
 
 export const flowerOfferings: FlowerOffering[] = [
   {
-    id: "tangerine-gerbera-bouquet",
-    name: "Tangerine Gerbera Bouquet",
+    id: "apricot-rose-amaranth-bouquet",
+    name: "Apricot Rose Amaranth Bouquet",
     description:
-      "Orange gerbera daisies with pink spray roses, purple veronica, and leafy greens.",
-    image: "/images/IMG_2338.JPG",
-    cardColor: "#ed2b82",
-  },
-  {
-    id: "blush-tulip-garden",
-    name: "Blush Tulip Garden",
-    description:
-      "Pink carnations, white tulips, green button mums, and sculptural curly willow.",
-    image: "/images/IMG_2413.JPG",
-    cardColor: "#c7da38",
-  },
-  {
-    id: "ruby-lily-bouquet",
-    name: "Ruby Lily Bouquet",
-    description:
-      "Stargazer lilies, orange roses, red roses, lily buds, and airy dried texture.",
-    image: "/images/IMG_2745.JPG",
+      "Apricot roses, white hydrangea, magenta spray roses, trailing amaranthus, and variegated greenery.",
+    image: "/images/apricot-rose-amaranth-bouquet.png",
     cardColor: "#f26a21",
   },
   {
-    id: "marigold-mum-bouquet",
-    name: "Marigold Mum Bouquet",
+    id: "green-amaranth-lily-garden",
+    name: "Green Amaranth Lily Garden",
     description:
-      "Yellow mums, yellow button mums, green hypericum berries, and airy bupleurum.",
-    image: "/images/IMG_3200.JPG",
-    cardColor: "#c7da38",
-  },
-  {
-    id: "magenta-lily-bouquet",
-    name: "Magenta Lily Bouquet",
-    description:
-      "Pink lilies, magenta stock, purple button mums, and deep burgundy mums.",
-    image: "/images/IMG_3376.JPG",
-    cardColor: "#ed2b82",
-  },
-  {
-    id: "pink-rose-hydrangea-dome",
-    name: "Pink Rose Hydrangea Dome",
-    description:
-      "Pink roses, blush orchids, green hydrangea, and a rounded garden silhouette.",
-    image: "/images/IMG_5415.JPG",
-    cardColor: "#ed2b82",
-  },
-  {
-    id: "burgundy-dahlia-mum-mix",
-    name: "Burgundy Dahlia Mum Mix",
-    description:
-      "Burgundy dahlias, magenta mums, green hydrangea, and fresh rose buds.",
-    image: "/images/IMG_5485.JPG",
-    cardColor: "#c7da38",
-  },
-  {
-    id: "raspberry-rose-garden",
-    name: "Raspberry Rose Garden",
-    description:
-      "Red roses, red gerbera daisies, magenta mums, waxflower, and rosemary stems.",
-    image: "/images/IMG_5946.JPG",
-    cardColor: "#ed2b82",
-  },
-  {
-    id: "candlelight-rose-centerpiece",
-    name: "Candlelight Rose Centerpiece",
-    description:
-      "Peach roses, burgundy mums, gerbera daisies, statice, and moody seasonal foliage.",
-    image: "/images/IMG_6258.JPG",
-    cardColor: "#f26a21",
-  },
-  {
-    id: "blue-iris-lily-bouquet",
-    name: "Blue Iris Lily Bouquet",
-    description:
-      "Blue iris, white tulips, white stock, white mums, and crisp green stems.",
-    image: "/images/IMG_7183.JPG",
+      "Green hydrangea, lily buds, purple garden blooms, hanging amaranthus, curly willow, and variegated foliage.",
+    image: "/images/green-amaranth-lily-garden.png",
     cardColor: "#344f20",
   },
   {
-    id: "pink-rose-garden-dome",
-    name: "Pink Rose Garden Dome",
+    id: "orange-rose-hydrangea-bouquet",
+    name: "Orange Rose Hydrangea Bouquet",
     description:
-      "Pink roses and green hydrangea gathered into a compact rounded bouquet.",
-    image: "/images/IMG_7251.JPG",
+      "Orange roses, white hydrangea, ornithogalum, bupleurum, and green textural stems.",
+    image: "/images/sage-ribbon-vase.png",
     cardColor: "#c7da38",
   },
   {
-    id: "purple-mum-hydrangea-dome",
-    name: "Purple Mum Hydrangea Dome",
+    id: "pink-star-lily-bouquet",
+    name: "Pink Star Lily Bouquet",
     description:
-      "Purple cushion mums, lavender mums, green hydrangea, and a clean rounded shape.",
-    image: "/images/IMG_7949.JPG",
+      "Pink star lilies, magenta button mums, white lily buds, and leafy green stems.",
+    image: "/images/IMG_8177.JPG",
+    cardColor: "#ed2b82",
+  },
+  {
+    id: "peach-rose-hydrangea-bouquet",
+    name: "Peach Rose Hydrangea Bouquet",
+    description:
+      "Peach roses, white hydrangea, white chamomile, and soft cream filler.",
+    image: "/images/IMG_8049.JPG",
+    cardColor: "#f26a21",
+  },
+  {
+    id: "citrus-rose-mum-bouquet",
+    name: "Citrus Rose Mum Bouquet",
+    description:
+      "Yellow roses, orange roses, orange mums, and fresh green filler.",
+    image: "/images/IMG_9150.JPG",
+    cardColor: "#f26a21",
+  },
+  {
+    id: "autumn-safflower-garden",
+    name: "Autumn Safflower Garden",
+    description:
+      "Orange safflower, rust mums, eucalyptus, seeded greenery, and a small accent posy.",
+    image: "/images/IMG_8877.JPG",
+    cardColor: "#c7da38",
+  },
+  {
+    id: "autumn-mum-centerpiece",
+    name: "Autumn Mum Centerpiece",
+    description:
+      "Bronze football mums, green button mums, yellow button mums, and warm fall texture.",
+    image: "/images/IMG_8825.JPG",
+    cardColor: "#c7da38",
+  },
+  {
+    id: "pink-gladiolus-garden",
+    name: "Pink Gladiolus Garden",
+    description:
+      "Pink gladiolus, peach roses, yellow ranunculus, green hydrangea, and eucalyptus.",
+    image: "/images/IMG_8581.JPG",
+    cardColor: "#f26a21",
+  },
+  {
+    id: "orange-lily-amaranth-bouquet",
+    name: "Orange Lily Amaranth Bouquet",
+    description:
+      "Orange lilies, burgundy hanging amaranthus, strawflowers, and bronzy foliage.",
+    image: "/images/IMG_8278.JPG",
+    cardColor: "#ed2b82",
+  },
+  {
+    id: "orange-gerbera-hydrangea-mix",
+    name: "Orange Gerbera Hydrangea Mix",
+    description:
+      "Orange gerbera daisies, green hydrangea, hot-pink mums, and button mums.",
+    image: "/images/IMG_8057.JPG",
     cardColor: "#ed2b82",
   },
   {
@@ -113,91 +105,99 @@ export const flowerOfferings: FlowerOffering[] = [
     cardColor: "#f26a21",
   },
   {
-    id: "orange-gerbera-hydrangea-mix",
-    name: "Orange Gerbera Hydrangea Mix",
+    id: "purple-mum-hydrangea-dome",
+    name: "Purple Mum Hydrangea Dome",
     description:
-      "Orange gerbera daisies, green hydrangea, hot-pink mums, and button mums.",
-    image: "/images/IMG_8057.JPG",
+      "Purple cushion mums, lavender mums, green hydrangea, and a clean rounded shape.",
+    image: "/images/IMG_7949.JPG",
     cardColor: "#ed2b82",
   },
   {
-    id: "orange-lily-amaranth-bouquet",
-    name: "Orange Lily Amaranth Bouquet",
+    id: "pink-rose-garden-dome",
+    name: "Pink Rose Garden Dome",
     description:
-      "Orange lilies, burgundy hanging amaranthus, strawflowers, and bronzy foliage.",
-    image: "/images/IMG_8278.JPG",
-    cardColor: "#ed2b82",
-  },
-  {
-    id: "pink-gladiolus-garden",
-    name: "Pink Gladiolus Garden",
-    description:
-      "Pink gladiolus, peach roses, yellow ranunculus, green hydrangea, and eucalyptus.",
-    image: "/images/IMG_8581.JPG",
-    cardColor: "#f26a21",
-  },
-  {
-    id: "autumn-mum-centerpiece",
-    name: "Autumn Mum Centerpiece",
-    description:
-      "Bronze football mums, green button mums, yellow button mums, and warm fall texture.",
-    image: "/images/IMG_8825.JPG",
+      "Pink roses and green hydrangea gathered into a compact rounded bouquet.",
+    image: "/images/IMG_7251.JPG",
     cardColor: "#c7da38",
   },
   {
-    id: "autumn-safflower-garden",
-    name: "Autumn Safflower Garden",
+    id: "blue-iris-lily-bouquet",
+    name: "Blue Iris Lily Bouquet",
     description:
-      "Orange safflower, rust mums, eucalyptus, seeded greenery, and a small accent posy.",
-    image: "/images/IMG_8877.JPG",
-    cardColor: "#c7da38",
-  },
-  {
-    id: "citrus-rose-mum-bouquet",
-    name: "Citrus Rose Mum Bouquet",
-    description:
-      "Yellow roses, orange roses, orange mums, and fresh green filler.",
-    image: "/images/IMG_9150.JPG",
-    cardColor: "#f26a21",
-  },
-  {
-    id: "peach-rose-hydrangea-bouquet",
-    name: "Peach Rose Hydrangea Bouquet",
-    description:
-      "Peach roses, white hydrangea, white chamomile, and soft cream filler.",
-    image: "/images/IMG_8049.JPG",
-    cardColor: "#f26a21",
-  },
-  {
-    id: "pink-star-lily-bouquet",
-    name: "Pink Star Lily Bouquet",
-    description:
-      "Pink star lilies, magenta button mums, white lily buds, and leafy green stems.",
-    image: "/images/IMG_8177.JPG",
-    cardColor: "#ed2b82",
-  },
-  {
-    id: "orange-rose-hydrangea-bouquet",
-    name: "Orange Rose Hydrangea Bouquet",
-    description:
-      "Orange roses, white hydrangea, ornithogalum, bupleurum, and green textural stems.",
-    image: "/images/sage-ribbon-vase.png",
-    cardColor: "#c7da38",
-  },
-  {
-    id: "green-amaranth-lily-garden",
-    name: "Green Amaranth Lily Garden",
-    description:
-      "Green hydrangea, lily buds, purple garden blooms, hanging amaranthus, curly willow, and variegated foliage.",
-    image: "/images/green-amaranth-lily-garden.png",
+      "Blue iris, white tulips, white stock, white mums, and crisp green stems.",
+    image: "/images/IMG_7183.JPG",
     cardColor: "#344f20",
   },
   {
-    id: "apricot-rose-amaranth-bouquet",
-    name: "Apricot Rose Amaranth Bouquet",
+    id: "candlelight-rose-centerpiece",
+    name: "Candlelight Rose Centerpiece",
     description:
-      "Apricot roses, white hydrangea, magenta spray roses, trailing amaranthus, and variegated greenery.",
-    image: "/images/apricot-rose-amaranth-bouquet.png",
+      "Peach roses, burgundy mums, gerbera daisies, statice, and moody seasonal foliage.",
+    image: "/images/IMG_6258.JPG",
     cardColor: "#f26a21",
+  },
+  {
+    id: "raspberry-rose-garden",
+    name: "Raspberry Rose Garden",
+    description:
+      "Red roses, red gerbera daisies, magenta mums, waxflower, and rosemary stems.",
+    image: "/images/IMG_5946.JPG",
+    cardColor: "#ed2b82",
+  },
+  {
+    id: "burgundy-dahlia-mum-mix",
+    name: "Burgundy Dahlia Mum Mix",
+    description:
+      "Burgundy dahlias, magenta mums, green hydrangea, and fresh rose buds.",
+    image: "/images/IMG_5485.JPG",
+    cardColor: "#c7da38",
+  },
+  {
+    id: "pink-rose-hydrangea-dome",
+    name: "Pink Rose Hydrangea Dome",
+    description:
+      "Pink roses, blush orchids, green hydrangea, and a rounded garden silhouette.",
+    image: "/images/IMG_5415.JPG",
+    cardColor: "#ed2b82",
+  },
+  {
+    id: "magenta-lily-bouquet",
+    name: "Magenta Lily Bouquet",
+    description:
+      "Pink lilies, magenta stock, purple button mums, and deep burgundy mums.",
+    image: "/images/IMG_3376.JPG",
+    cardColor: "#ed2b82",
+  },
+  {
+    id: "marigold-mum-bouquet",
+    name: "Marigold Mum Bouquet",
+    description:
+      "Yellow mums, yellow button mums, green hypericum berries, and airy bupleurum.",
+    image: "/images/IMG_3200.JPG",
+    cardColor: "#c7da38",
+  },
+  {
+    id: "ruby-lily-bouquet",
+    name: "Ruby Lily Bouquet",
+    description:
+      "Stargazer lilies, orange roses, red roses, lily buds, and airy dried texture.",
+    image: "/images/IMG_2745.JPG",
+    cardColor: "#f26a21",
+  },
+  {
+    id: "blush-tulip-garden",
+    name: "Blush Tulip Garden",
+    description:
+      "Pink carnations, white tulips, green button mums, and sculptural curly willow.",
+    image: "/images/IMG_2413.JPG",
+    cardColor: "#c7da38",
+  },
+  {
+    id: "tangerine-gerbera-bouquet",
+    name: "Tangerine Gerbera Bouquet",
+    description:
+      "Orange gerbera daisies with pink spray roses, purple veronica, and leafy greens.",
+    image: "/images/IMG_2338.JPG",
+    cardColor: "#ed2b82",
   },
 ];
